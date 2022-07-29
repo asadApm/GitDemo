fc4df6dd429f86c7a4ed797bf1e296982c8e70bb
asad <asad-saleem@hotmail.co.uk>
2022-07-29 20:22:45 +0200

    first commit

---
+++ Mavenjava/src/test/java/QAExpert/Mavenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package QAExpert.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutmomation");
+		
+		
+	}
+	
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+		
+	}
+
+}
